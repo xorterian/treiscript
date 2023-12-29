ddd1bd3a695b999fd7dfd7cf553f2f630f7612ba
Kristóf Vörös <vkristof211@gmail.com>
2023-12-29 02:09:55 +0100

    Def function making expressions connected with relations

--- init-basic-rels-and-logics.py
+++ init-basic-rels-and-logics.py
@@ -163,3 +163,70 @@ def log_bin(code, rectangle=False, radix=0):
   dr.append(d_p3)
   dr.append(d_p4)
   return dr
+
+def make_rel(A,R,B):
+  padding = dx/7
+
+  if not isinstance(R,list) or not isinstance(B,list):
+    dim_A = (A.width, A.height)
+    dim_R = (R.width, R.height)
+    dim_B = (B.width, B.height)
+    d = draw.Drawing(dim_A[0]+dim_R[0]+dim_B[0]+2*padding,max(dim_A[1],max(dim_R[1],dim_B[1]))+2*padding)
+    group_A = draw.Group()
+    for e in copy.deepcopy(A.all_elements()):
+      group_A.append(e)
+    group_R = draw.Group()
+    for e in copy.deepcopy(R.all_elements()):
+      group_R.append(e)
+    group_B = draw.Group()
+    for e in copy.deepcopy(B.all_elements()):
+      group_B.append(e)
+
+    offset_y_2B = B.height-A.height if A.height < B.height else 0
+    offset_y_2A = A.height-B.height if A.height > B.height else 0
+    d.append(draw.Use(group_A,padding,padding+offset_y_2B))
+    d.append(draw.Use(group_R,padding+dim_A[0],padding/2 + d.height/2 - R.height/2))
+    d.append(draw.Use(group_B,padding+dim_A[0]+dim_R[0],padding + offset_y_2A))
+  
+  else:
+    n = 1
+    if isinstance(R,list) and not isinstance(B,list):
+      n = len(R)
+      B = [B] * n
+    elif not isinstance(R,list) and isinstance(B,list):
+      n = len(B)
+      R = [R] * n
+    else:
+      n = len(R)
+    dims = [(A.width, A.height)]
+    for i in range(n):
+      dims.append((R[i].width,R[i].height))
+      dims.append((B[i].width,B[i].height))
+    width = sum([ wid[0] for wid in dims ]) + 2*(n+1)*padding
+    height = max([ hei[1] for hei in dims ])
+
+    d = draw.Drawing(width,height+2*padding)
+    group_A = draw.Group()
+    for e in copy.deepcopy(A.all_elements()):
+      group_A.append(e)
+    group_Rs = []
+    group_Bs = []
+    for i in range(n):
+      group_R = draw.Group()
+      for e in copy.deepcopy(R[i].all_elements()):
+        group_R.append(e)
+      group_Rs.append(group_R)
+      group_B = draw.Group()
+      for e in copy.deepcopy(B[i].all_elements()):
+        group_B.append(e)
+      group_Bs.append(group_B)
+
+    d.append(draw.Use(group_A,padding,height/2-A.height/2+padding))
+    L = padding+A.width+padding
+    for i in range(n):
+      d.append(draw.Use(group_Rs[i],L,height/2-R[i].height/2+padding))
+      L += R[i].width + padding
+      d.append(draw.Use(group_Bs[i],L,height/2-B[i].height/2+padding))
+      L += B[i].width + padding
+
+  return d
